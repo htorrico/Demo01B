f4472c0caec477aa2a34af62d0c106a4112cbdb8
hugo torrico <htorrico@tecsup.edu.pe>
2024-08-20 19:51:16 -0500

    Agregar archivos de proyecto.

---
+++ Demo01B/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    public class Persona
+    {
+        public Persona()
+        {
+
+        }
+        public Persona(string nombres, string apellidos)
+        {
+            Nombres = nombres;
+            Apellidos = apellidos;
+        }
+
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+
+        public string NombresCompletos { get; set; }
+
+        public string DevolverEstado()
+        {
+            string mensaje = "Es menor de edad";
+            if (Edad > 18)
+                mensaje = "Es mayor de edad";
+
+            return mensaje;
+        }
+
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Nombres + " " + Apellidos;
+
+        }
+    }
+}
